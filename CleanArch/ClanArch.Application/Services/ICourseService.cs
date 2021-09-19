19da09435f0372f2f6d4d842fbd0e07c75207a48
Sevak Abedi <send2sev@live.com>
2021-09-19 15:47:29 -0700

    Commit Service and repository interfaces, CourseViewModel

---
+++ CleanArch/ClanArch.Application/Services/ICourseService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using ClanArch.Application.ViewModels;
+
+namespace ClanArch.Application.Services
+{
+    public interface ICourseService
+    {
+        IEnumerable<CourseViewModel> GetCourses();
+    }
+}
---
+++ CleanArch/ClanArch.Application/ViewModels/CourseViewModel.cs
@@ -0,0 +1,10 @@
+﻿using CleanArch.Domain.Models;
+using System.Collections.Generic;
+
+namespace ClanArch.Application.ViewModels
+{
+    public class CourseViewModel
+    {
+        public IEnumerable<Course> Courses { get; set; }
+    }
+}
